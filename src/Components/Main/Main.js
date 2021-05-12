ff9ba6f6d7d17231eee6437b0b29623a01aea0a1
tarunraghav226 <tarun.raghav226@gmail.com>
2021-05-12 22:53:46 +0530

    Added styles in main.

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,8 +1,14 @@
 import React from "react";
 
+import TopNavigation from '../Navigation/TopNavigation/TopNavigation';
+import styles from './Main.module.css';
+
 const Main = () =>{
     return(
-        <>
-        </>
+        <div className={styles.Main}>
+            <TopNavigation/>    
+        </div>
     );
 }
+
+export default Main;
