8ea17d58aefca784d225dfc0c69ff43a1fce7040
tarunraghav226 <tarun.raghav226@gmail.com>
2021-09-14 20:00:13 +0530

    Fixed style.

--- src/Components/Navigation/TopNavigation/TopNavigation.js
+++ src/Components/Navigation/TopNavigation/TopNavigation.js
@@ -1,20 +1,27 @@
-import React,{Component} from 'react';
+import React, { Component } from "react";
 
-import styles from './TopNavigation.module.css';
+import styles from "./TopNavigation.module.css";
 
+import LoginModal from "../../Modals/LoginModal";
 
-class TopNavigation extends Component{
-    render(){
-        return (
-            <div className={styles.Header}>
-                <div className={styles.Link}><a href="/">NEWS</a></div>
-                <div className={styles.Links}>
-                    <span className={styles.Link}><a href="/">Home</a></span>
-                    <span className={styles.Link}><a href="/">More NEWS</a></span>
-                </div>
-            </div>
-        );
-    }
+class TopNavigation extends Component {
+  render() {
+    return (
+      <div className={styles.Header}>
+        <div className={styles.Link}>
+          <a href="/">NEWS</a>
+        </div>
+        <div className={styles.Links}>
+          <span className={styles.Link}>
+            <a href="/">Home</a>
+          </span>
+          <span className={styles.Link}>
+            <a href="/">More NEWS</a>
+          </span>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default TopNavigation;
